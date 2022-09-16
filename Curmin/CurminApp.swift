9341f0177b68871d90f201111a084e4eee091f23
Tarik Yasar <m.tarikyasar@gmail.com>
2022-09-16 22:00:23 +0300

    Initial Commit

---
+++ Curmin/CurminApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurminApp.swift
+//  Curmin
+//
+//  Created by Tarik Yasar on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CurminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
