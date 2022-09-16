7f04c9d04584ebbb668d4fbd2b03a3cd5b188594
Tarik Yasar <m.tarikyasar@gmail.com>
2022-09-16 22:18:24 +0300

    Initial commit

--- Curmin/ContentView.swift
+++ Curmin/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        HStack {
+            Image(systemName: "dollarsign.circle")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+			
+            Text("Curmin")
         }
         .padding()
     }
